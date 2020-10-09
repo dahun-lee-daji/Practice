c3fe256a97fbd82663137bc9e2e31a8bcf416298
dahun-lee-daji <ssss6596@gmail.com>
2020-10-10 03:14:29 +0900

    tableview과 cell 연결

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    //UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    //UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath)")
     }
     
 
